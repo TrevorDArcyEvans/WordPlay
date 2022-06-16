fc44139f5de760ae5c35ee45ee4ab247d5451453
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2022-06-16 21:40:44 +0100

    - code cleanup

--- WordPlay/Program.cs
+++ WordPlay/Program.cs
@@ -88,41 +88,36 @@ namespace WordPlay
       }
     }
 
-    public static void ConfigExperiments(List<ExperimentStruct> E, int defaultLength = 6)
+    private static void ConfigExperiments(List<ExperimentStruct> exps, int defaultLength = 6)
     {
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
           new PlayStruct { Type = PlayerType.basic, Name = "Basic", Gamelength = defaultLength }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
           new PlayStruct { Type = PlayerType.dopey, Name = "Dopey", Gamelength = defaultLength }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
           new PlayStruct { Type = PlayerType.dreamy, Name = "Dreamy", Gamelength = defaultLength }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
           new PlayStruct { Type = PlayerType.thoughtful, Name = "Thoughtful", Gamelength = defaultLength }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
           new PlayStruct { Type = PlayerType.sage, Name = "Sage", Gamelength = defaultLength }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
           new PlayStruct { Type = PlayerType.picky, Name = "Picky - Banana", Gamelength = defaultLength, Seed = "learn;rough;aioli;route;louse;rouge;think;drink;cramp" }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
          new PlayStruct { Type = PlayerType.picky, Name = "Picky - Ravenclaw", Gamelength = defaultLength, Seed = "adore;spiny;audio;stern" }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
          new PlayStruct { Type = PlayerType.picky, Name = "Picky - word.tips", Gamelength = defaultLength, Seed = "Noise,Abuse,Opera,Naive,About,Piano,House,Alone,Above,Email,Azure,Juice,Movie,Cause,Video,Quiet,Olive,Ocean,Alive,Value,Voice,Radio,Media,ludic,ulnar,daunt,acidy".ToLower() }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
          new PlayStruct { Type = PlayerType.picky, Name = "Picky - Vanna", Gamelength = defaultLength, Seed = "terns,ducal,aphid,homed" }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
          new PlayStruct { Type = PlayerType.picky, Name = "Picky - Ace", Gamelength = defaultLength, Seed = "laser" }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
          new PlayStruct { Type = PlayerType.picky, Name = "Picky - Bean", Gamelength = defaultLength, Seed = "frisk,empty,gland,picky,wrest,buxom,judge,proxy,snack,blitz,blind,quoth,jerky,swamp,blind,mucky,topaz,shrew,bugle,frock,nymph,vista,album,fjord,sixty,wench,album,fjord,winch,zesty,dunce,morph,gawky,blitz,glyph,banjo,wreck,midst" }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
          new PlayStruct { Type = PlayerType.picky, Name = "Picky - Sonar", Gamelength = defaultLength, Seed = "outie,yarns" }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
           new PlayStruct { Type = PlayerType.freaky, Name = "Freaky", Gamelength = defaultLength }));
-      E.Add(new ExperimentStruct(
+      exps.Add(new ExperimentStruct(
           new PlayStruct { Type = PlayerType.doublefreaky, Name = "DoubleFreak", Gamelength = defaultLength }));
     }
-
-  } //end class
-
-
-
-
+  }
 }
